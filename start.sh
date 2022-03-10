d5e8c2dadf33d92005a6352c4a99d466cdafb56e
goberomsu <310o@naver.com>
2022-03-10 18:53:40 +0900

    add : scripts

--- start.sh
+++ start.sh
@@ -5,10 +5,14 @@ AfterBlog=D:/goberomsu.github.io/content
 
 for post in $BeforeBlog/*
 do
-
-    sed -i '/date/d' $post
-    sed -e "5 i\\date: '2022-03-10 18:00:00'" -i $post
-    sed -e "5 i\\author: 범수" -i $post
+    # sed -i '/categories:/,+4 d' $post
+    # sed -i '/tags:/,+1 d' $post
+    # sed -i '/date/d' $post
+    # sed -e "2 i\\emoji: 🏃" -i $post
+    # sed -e "3 i\\categories: 블로그" -i $post
+    # sed -e "5 i\\tags: 블로그" -i $post
+    # sed -e "5 i\\date: '2022-03-10 18:00:00'" -i $post
+    # sed -e "5 i\\author: 범수" -i $post
     postName=${post:24:-3}
     cd $AfterBlog
     mkdir $postName
