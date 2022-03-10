dadb752f7510ec41b3e20acf69b48512aca12459
goberomsu <310o@naver.com>
2022-03-10 19:32:03 +0900

    add : category script

--- start.sh
+++ start.sh
@@ -3,9 +3,11 @@
 BeforeBlog=D:/MyBlog/source/_posts
 AfterBlog=D:/goberomsu.github.io/content
 
-for post in $BeforeBlog/*
+cd $AfterBlog
+
+for post in $AfterBlog/*
 do
-    sed -i '/categories:/d' $post
+    # sed -i '/categories:/d' $post
     # sed -i '/tags:/,+1 d' $post
     # sed -i '/date/d' $post
     # sed -e "2 i\\emoji: 🏃" -i $post
@@ -13,8 +15,16 @@ do
     # sed -e "5 i\\tags: 블로그" -i $post
     # sed -e "5 i\\date: '2022-03-10 18:00:00'" -i $post
     # sed -e "5 i\\author: 범수" -i $post
-    postName=${post:24:-3}
-    cd $AfterBlog
-    mkdir $postName
-    mv $post $postName/
+    postName=${post:31}
+    name='til'
+    # echo $postName
+    if [[ "${postName}" == *${name}* ]];then
+        cd $postName
+        # sed -i '/categories:/d' ${postName}.md
+        sed -e "3 i\\categories: $name" -i ${postName}.md
+        cd ..
+    fi
+    # cd $AfterBlog
+    # mkdir $postName
+    # mv $post $postName/
 done
